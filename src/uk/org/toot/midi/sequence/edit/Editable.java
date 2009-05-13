de50233830b8b174cb77929ad9e8aba37bb5b2d5
TootSoftware <TootSoftware@79a64468-052e-0410-9386-550f7274125b>
2009-05-13 13:27:18 +0000

    some packages moved to tootproject and tootmidiseq

--- src/uk/org/toot/midi/sequence/edit/Editable.java
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2005 Steve Taylor (toot.org.uk) */
-
-package uk.org.toot.midi.sequence.edit;
-
-public interface Editable
-{
-    Object clone() throws CloneNotSupportedException;
-}
--- src/uk/org/toot/midi/sequence/edit/Moveable.java
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2005 Steve Taylor (toot.org.uk) */
-
-package uk.org.toot.midi.sequence.edit;
-
-public interface Moveable extends Editable
-{
-    boolean move(long ticks, int semitones);
-}
